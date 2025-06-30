486cd220ebe18c5a60aef6dd4343f783af643db7
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2025-06-30 07:53:55 +0000

    talk to pico

---
+++ ttp.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CPI_DIR="/Volumes/CIRCUITPY"
+
+GIT_DIR_CODE="$SCRIPT_DIR/code"
+GIT_DIR_SCRIPTS="$GIT_DIR_CODE/scripts"
+GIT_DIR_LIB="$GIT_DIR_CODE/lib"
+
+CPI_DIR_CODE="$CPI_DIR"
+CPI_DIR_SCRIPTS="$CPI_DIR/scripts"
+CPI_DIR_LIB="$CPI_DIR/scripts"
+
+
+arg=$1
+
+if [[ "$arg" == "to_pico" ]];then
+	echo "foo"
+
+elif [[ "$arg" == "to_git" ]];then
+	echo "cp $CPI_DIR_CODE/s*py $GIT_DIR_CODE"
+	echo "cp $CPI_DIR_SCRIPT/*scode $GIT_DIR_SCRIPTS"
+	echo "cp $CPI_DIR_LIB/s*py $GIT_DIR_LIB"
+else
+	echo "no arg provided"
+	exit 1
+fi
+
+
+
