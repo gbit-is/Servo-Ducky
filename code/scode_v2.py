2778a3a5e366e98cbf1734eb71951784bf2f8130
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2025-07-01 06:46:20 +0000

    add WIP V2

---
+++ code/scode_v2.py
@@ -0,0 +1,28 @@
+import board
+import busio
+import time
+import os
+
+import scode
+import asyncio
+from adafruit_pca9685 import PCA9685
+
+
+
+
+SCL_PIN = board.GP1
+SDA_PIN = board.GP0
+PCA_FREQ = 60
+PCA_DUTY_CYCLE = 0x7FFF
+NUMBER_OF_SERVOS = 4
+
+# Create the I2C bus interface.
+i2c = busio.I2C(SCL_PIN,SDA_PIN)    # Pi Pico RP2040
+pca = PCA9685(i2c)
+pca.frequency = PCA_FREQ
+pca.channels[0].duty_cycle = PCA_DUTY_CYCLE
+
+
+s = scode.scode(pca=pca)
+
+x = s.servos["1"]["servo"].angle = 0
