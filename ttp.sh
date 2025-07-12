2106fab22343fa4cd28289d49a2b5913e6eaeb86
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2025-07-12 09:23:25 +0000

    sync code.py as well

--- ttp.sh
+++ ttp.sh
@@ -25,6 +25,7 @@ if [[ "$arg" == "to_pico" ]];then
 elif [[ "$arg" == "to_git" ]];then
 	cp $CPI_DIR_CODE/s*py $GIT_DIR_CODE
 	cp $CPI_DIR_CODE/boot.py $GIT_DIR_CODE
+	cp $CPI_DIR_CODE/code.py $GIT_DIR_CODE
 	cp $CPI_DIR_CODE/s*toml $GIT_DIR_CODE
 	cp $CPI_DIR_SCRIPTS/*scode $GIT_DIR_SCRIPTS
 	cp $CPI_DIR_LIB/s*py $GIT_DIR_LIB
